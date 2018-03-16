c4909c11b82782e65429ebd4d701f9afd83048c2
hzwangchenyan <hzwangchenyan@corp.netease.com>
2018-03-16 16:20:49 +0800

    后退

--- app/src/main/java/me/wcy/androidweekly/activity/BrowserActivity.kt
+++ app/src/main/java/me/wcy/androidweekly/activity/BrowserActivity.kt
@@ -4,10 +4,13 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.PixelFormat
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import android.widget.ProgressBar
 import com.tencent.smtt.sdk.WebChromeClient
+import com.tencent.smtt.sdk.WebSettings
 import com.tencent.smtt.sdk.WebView
+import com.tencent.smtt.sdk.WebViewClient
 import me.wcy.androidweekly.R
 import me.wcy.androidweekly.constants.Extras
 import me.wcy.androidweekly.utils.binding.Bind
@@ -36,8 +39,20 @@ class BrowserActivity : BaseActivity() {
         val url = intent.getStringExtra(Extras.URL)
 
         setTitle(title)
-        webView!!.webChromeClient = webChromeClient
+        supportActionBar!!.setHomeAsUpIndicator(R.drawable.ic_menu)
+        webView!!.settings.setSupportZoom(true)
+        webView.settings.layoutAlgorithm = WebSettings.LayoutAlgorithm.SINGLE_COLUMN
+        webView.settings.loadWithOverviewMode = true
+        webView.webViewClient = webViewClient
+        webView.webChromeClient = webChromeClient
         webView.loadUrl(url)
+    }
+
+    private val webViewClient = object : WebViewClient() {
+        override fun shouldOverrideUrlLoading(p0: WebView?, p1: String?): Boolean {
+            p0!!.loadUrl(p1)
+            return true
+        }
     }
 
     private val webChromeClient = object : WebChromeClient() {
@@ -45,6 +60,22 @@ class BrowserActivity : BaseActivity() {
             super.onProgressChanged(p0, p1)
             progressBar!!.progress = p1
             progressBar.visibility = if (p1 == 100) View.GONE else View.VISIBLE
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item!!.itemId == android.R.id.home) {
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        if (webView!!.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
         }
     }
 }
